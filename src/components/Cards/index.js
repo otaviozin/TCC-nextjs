1620f9222e5ba0abddc6d143a6c8eb91f824816e
Otávio Pereira <60949367+otaviozin@users.noreply.github.com>
2022-07-22 15:12:45 -0300

    Link changes

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -45,12 +45,10 @@ export default function Cards(props){
         return(
             <div className='flex 2xl:grid 2xl:grid-cols-3 flex-wrap justify-center xl:justify-around gap-2 2xl:gap-x-36 2xl:gap-y-20 py-7'>
                 {char.local.map(local => (
-                    <Link href={`/local/${local.name}`} passHref key={local.id}>
-                        <div className='transition ease-out hover:bg-gradient-to-t from-red-900 via-gray-primary to-gray-cards hover:scale-110 duration-150 bg-gray-cards w-fit px-7 py-5 mt-3 rounded-md text-center font-bold text-xl cursor-pointer'>
-                            <Image src='https://freesvg.org/img/abstract-user-flat-4.png' alt='Character image' width={100} height={100} priority='true' />
-                            <h3 className='mt-4'>{local.name}</h3>
-                        </div>
-                    </Link>
+                    <div key={local.id} className='transition ease-out hover:bg-gradient-to-t from-red-900 via-gray-primary to-gray-cards hover:scale-110 duration-150 bg-gray-cards w-fit px-7 py-5 mt-3 rounded-md text-center font-bold text-xl cursor-pointer'>
+                        <Image src='https://freesvg.org/img/abstract-user-flat-4.png' alt='Character image' width={100} height={100} priority='true' />
+                        <h3 className='mt-4'>{local.name}</h3>
+                    </div>
                 ))}
             </div>
         );
